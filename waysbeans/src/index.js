138f4c5b3038904bb47b63b46735497a237c25ee
Ade Fitriana <adefitriana007@gmail.com>
2022-09-28 22:31:43 +0700

    update

--- waysbeans/src/index.js
+++ waysbeans/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { UserContextProvider } from "./context/useContext";
 import { QueryClient, QueryClientProvider } from "react-query";
@@ -20,8 +19,3 @@ root.render(
     </UserContextProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
